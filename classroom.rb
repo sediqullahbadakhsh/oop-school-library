35bae90a0e7caeb513f39acf99741fc26adc3fdf
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-16 22:16:01 +0430

    fix linters

--- classroom.rb
+++ classroom.rb
@@ -1,13 +1,13 @@
 class Classroom
-    attr_accessor :students, :label
-  
-    def initialize(label)
-      @label = label
-      @students = []
-    end
-  
-    def add_student(student)
-      students.push(student)
-      student.classroom = self
-    end
+  attr_accessor :students, :label
+
+  def initialize(label)
+    @label = label
+    @students = []
   end
+
+  def add_student(student)
+    students.push(student)
+    student.classroom = self
+  end
+end
