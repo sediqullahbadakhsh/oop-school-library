e3d345762e80198e570bc6740ae60565c928bbd3
Have Samuel <samiehave9@gmail.com>
2022-06-24 01:24:58 +0300

    Added the rspec test to the student_spec file

--- SpecData/student_spec.rb
+++ SpecData/student_spec.rb
@@ -1,13 +1,24 @@
 require_relative '../student'
 
 describe Student do 
-  context “When testing the HelloWorld class” do 
-     
-     it "The say_hello method should return 'Hello World'" do 
-        hw = HelloWorld.new 
-        message = hw.say_hello 
-        expect(message).to eq "Hello World!" 
+  context 'It should create a student' do
+   id = 13
+   age = 28
+   name = 'Walker'
+   classroom = '4 red'
+   parent_permission = true
+   student = Student.new(id, age, name, classroom, parent_permission)
+
+     it 'should create an instance of class student' do
+        expect(student).to be_an_instance_of(Student)
      end
-     
+
+     it 'should check student details' do
+      expect(student.id).to eq(id)
+      expect(student.age).to eq(age)
+      expect(student.name).to eq(name)
+      expect(student.classroom).to eq(classroom)
+      expect(student.parent_permission).to eq(parent_permission)
+     end
   end 
 end
