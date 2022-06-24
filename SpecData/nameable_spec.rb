e731c5b9ecebe548221ad7572a07db176c2973d6
Have Samuel <samiehave9@gmail.com>
2022-06-24 05:30:51 +0300

    Fix Linters

--- SpecData/nameable_spec.rb
+++
@@ -1,6 +0,0 @@
-# require '../nameable'
-
-# describe Nameable do
-#   content 'Throws NotImplementedError when calling the base correct_name mathod of Nameable' do
-#     it 'Throws a NotImplementedError' do
-#       expect { Nameable.new.correct_name }.to
